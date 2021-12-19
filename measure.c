51880f1757ebdb0afac87953cb81bd7d05fccecc
GeroNir <nirgeron@yahoo.com>
2021-12-19 21:56:44 +0200

    day 1

---
+++ measure.c
@@ -0,0 +1,97 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <time.h>
+#define SIZE 1260785
+#define BILLION 1000000000.0
+
+int fsize(FILE *fp) {
+    int prev = ftell(fp);
+    fseek(fp, 0L, SEEK_END);
+    int sz = ftell(fp);
+    fseek(fp, prev, SEEK_SET); //go back to where we were
+    return sz;
+}
+
+void write_file(int sockf) {
+    struct timespec start, end;
+    clock_gettime(CLOCK_REALTIME, &start);
+    int n = 0;
+    int num = 0;
+    char buffer[SIZE];
+    FILE *fp;
+    int tmp;
+    char *filename = "bigfile.txt";
+    fp = fopen(filename, "r");
+    int size = fsize(fp);
+    int count = 0;
+    while (1) {
+        tmp = n;
+        n = recv(sockf, buffer, SIZE, 0);
+        if (tmp != n) {
+        	count++;
+        }
+        
+        if (n <= 0) {
+            break;
+        }
+        
+        if (count == 5) {
+            clock_gettime(CLOCK_REALTIME, &end);
+            double time_spent = (end.tv_sec - start.tv_sec) +
+                                (end.tv_nsec - start.tv_nsec) / BILLION;
+            printf("Time for transfer by cubic is: %f \n", time_spent);
+            clock_gettime(CLOCK_REALTIME, &start);
+        }
+        num = num + strlen(buffer);
+        if (num >= size) {
+            num = num - size;
+        }
+        bzero(buffer, SIZE);
+    }
+    clock_gettime(CLOCK_REALTIME, &end);
+    double time_spent = (end.tv_sec - start.tv_sec) +
+                        (end.tv_nsec - start.tv_nsec) / BILLION;
+    printf("Time for transfer by reno is: %f \n", time_spent);
+    return;
+}
+
+int main() {
+    char *ip = "127.0.0.1";
+    int port = 10000;
+    int sockf, new_sock;
+    struct sockaddr_in server_addr, new_addr;
+    socklen_t addr_size;
+    char buffer[SIZE];
+
+    sockf = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockf < 0) {
+        perror("socket error");
+        exit(1);
+    }
+    printf("Server socket created successfully.\n");
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = port;
+    server_addr.sin_addr.s_addr = inet_addr(ip);
+
+    int err = bind(sockf, (struct sockaddr *) &server_addr, sizeof(server_addr));
+    if (err < 0) {
+        perror("bind error");
+        exit(1);
+    }
+    printf("Binding successfully.\n");
+    if (listen(sockf, 10) == 0) {
+        printf("Connected...\n");
+    } else {
+        perror("Error in listening");
+        exit(1);
+    }
+    addr_size = sizeof(new_addr);
+    new_sock = accept(sockf, (struct sockaddr *) &new_addr, &addr_size);
+    write_file(new_sock);
+    printf("The file was received successfully");
+    return 0;
+}
---
+++ sender.c
@@ -0,0 +1,93 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#define SIZE 1260785
+
+void send_file(FILE *file, int sockf) {
+    int num = 0;
+    char data[SIZE] = {0};
+    while (fgets(data, SIZE, file) != NULL) {
+        if (send(sockf, data, SIZE, 0) == -1) {
+            perror("sending file error");
+            exit(1);
+        }
+        num = num + strlen(data);
+        bzero(data, SIZE);
+    }
+    printf("%d bytes were sent", num);
+}
+
+int main(int argc, char **argv) {
+    char buf[256];
+    socklen_t len;
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1) {
+        perror("socket");
+        return -1;
+    }
+
+    len = sizeof(buf);
+    if (getsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, buf, &len) != 0) {
+        perror("getsockopt");
+        return -1;
+    }
+
+    printf("Current: %s\n", buf);
+    char *ip = "127.0.0.1";
+    int port = 10000;
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = port;
+    server_addr.sin_addr.s_addr = inet_addr(ip);
+
+    int err = connect(sock, (struct sockaddr *) &server_addr, sizeof(server_addr));
+    if (err == -1) {
+        perror("socket error");
+        exit(1);
+    }
+    printf("Connected to Server.\n");
+    char *filename = "bigfile.txt";
+    for (int i = 0; i < 5; i++) {
+        FILE *file;
+        file = fopen(filename, "r");
+        if (file == NULL) {
+            perror("Error in reading file.");
+            exit(1);
+        }
+        send_file(file, sock);
+        fclose(file);
+        printf("File data sent successfully.\n");
+    }
+
+    strcpy(buf, "reno");
+    len = strlen(buf);
+    if (setsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, buf, len) != 0) {
+        perror("setsockopt");
+        return -1;
+    }
+    len = sizeof(buf);
+    if (getsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, buf, &len) != 0) {
+        perror("getsockopt");
+        return -1;
+    }
+    printf("New: %s\n", buf);
+    for (int i = 0; i < 5; i++) {
+        FILE *file;
+        file = fopen(filename, "r");
+        if (file == NULL) {
+            perror("reding file error");
+            exit(1);
+        }
+        send_file(file, sock);
+        fclose(file);
+        printf("File data sent successfully.\n");
+    }
+    printf("closing connection");
+    close(sock);
+    return 0;
+}
